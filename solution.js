05fd9808c78f24726ba6f729e4c23020fbe8060c
Freenerd <nospam@freenerd.de>
2015-01-24 18:36:01 +0100

    WIP

--- solution.js
+++ solution.js
@@ -41,6 +41,18 @@
             }
         }
         
+        function addToGlobalQueueUp(floorNum){
+            if (!_.contains(_this.globalQueueUp, floorNum)) {
+                _this.globalQueueUp.push(floorNum);
+            } 
+        }
+        
+        function addToGlobalQueueDown(floorNum){
+            if (!_.contains(_this.globalQueueDown, floorNum)) {
+                _this.globalQueueDown.push(floorNum);
+            } 
+        }
+        
         function elevatorFull(elevator) {
             return elevator.loadFactor() > 0.6;
         }       
@@ -61,6 +73,9 @@
 
         elevator.on("passing_floor", function() {
            // TODO: decide wether to stop or skip this floor, based on load
+            
+            //if(passingByUp(elevator, floor.floorNum()) && !elevatorFull(elevator)) {
+            //    addUp(elevator, floor.floorNum());
         });
 
         elevator.on("idle", function() {
@@ -90,26 +105,51 @@
         
         _.each(floors, function(floor) {
             floor.on("up_button_pressed", function() {
-                // extra behandlung von calls von floor 0
-                if(passingByUp(elevator, floor.floorNum()) && !elevatorFull(elevator)) {
-                    addUp(elevator, floor.floorNum());
-                } else {
-                    _this.globalQueueUp.push(floor.floorNum());
-                }
+                addToGlobalQueueUp(floor.floorNum());
             });
             
             floor.on("down_button_pressed", function() {
-                // extra behandlung von calls von floor MAX
-                if(passingByDown(elevator, floor.floorNum()) && !elevatorFull(elevator)) {
-                    addDown(elevator, floor.floorNum());
-                } else {
-                    _this.globalQueueDown.push(floor.floorNum());
-                }
+                addToGlobalQueueDown(floor.floorNum());
             });
         });
     },
     update: function(dt, elevators, floors) {
         var _this = this;
+        var elevator = elevators[0];
+        
+        if (elevator.destinationQueue.length === 0){
+            if (elevator.goingUpIndicator()){
+                var nextStops = _.select(_this.globalQueueUp, function(floorNum){
+                    return elevator.currentFloor <= floorNum;
+                });
+                //TODO continue here
+                console.log("eesdkfjskjdfhsdkjfh", elevator.destinationQueue);
+                elevator.destinationQueue += nextStops;
+                console.log(elevator.destinationQueue);
+            }
+            // nimm aus deiner Richtung den current florr und alles in guter richtung
+            // wechsel die richtung, nimme alles drunter liegt
+            // fehler 5 / 0 beachten
+            if (_this.globalQueueUp !== 0){
+                elevator.goingUpIndicator(true);                
+                elevator.goingDownIndicator(false);
+                
+                elevator.destinationQueue = _this.globalQueueUp;
+                _this.globalQueueUp = [];
+                elevator.destinationQueue.sort();
+                elevator.checkDestinationQueue(); 
+            } else {
+                elevator.goingUpIndicator(false);                
+                elevator.goingDownIndicator(true);
+                
+                elevator.destinationQueue = _this.globalQueueDown;
+                _this.globalQueueDown = [];
+                elevator.destinationQueue.sort();
+                elevator.destinationQueue.reverse();
+                elevator.checkDestinationQueue(); 
+            }
+        }
+        
         console.log("DT", dt, _this.globalQueueUp, _this.globalQueueDown); 
         elevators.forEach(function(elevator, index){
             console.log(index, elevator.destinationQueue, elevator.goingUpIndicator(), elevator.goingDownIndicator(), elevator.loadFactor());
